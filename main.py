2273264690c31c2722d513c3109b3e9c2c211a35
M. R. Hossain <rez182@gmail.com>
2022-06-03 23:36:06 -0400

    google news search is working

---
+++ main.py
@@ -0,0 +1,56 @@
+import pickle, sys
+from GoogleNews import GoogleNews
+
+
+def initialize(search):
+	try:
+		with open(search, 'rb') as file:
+			return pickle.load(file)
+	except:
+		googlenews = GoogleNews(period='1d', encode='utf-8')
+		googlenews.get_news(search)
+
+		Recursion_Limit = 0
+
+		while True:
+			Recursion_Limit += 1000
+			sys.setrecursionlimit(Recursion_Limit)
+			try:
+				with open(search, 'wb') as file:
+					pickle.dump(googlenews, file)
+				break
+			except:
+				pass
+
+		print("Success at Limit: " +str(Recursion_Limit))		
+		return googlenews
+		
+
+# print (sys.getrecursionlimit())
+
+# print (sys.getrecursionlimit())
+
+search = 'bitcoin'
+googlenews = initialize(search)
+
+
+# with open(search, 'rb') as file:
+# 	googlenews = pickle.load(file)
+
+# print(googlenews.get_links())
+
+# for link in googlenews.get_links():
+# 	print(link)
+
+# for title in googlenews.get_texts():
+# 	print(title)
+
+# print(googlenews.total_count())
+i = 1
+st = ""
+for res in googlenews.results():
+	# st += str(i) +" "
+	st += res['site'] +": "+ res['title'] +"\n"
+	# i += 1
+
+print(st)
